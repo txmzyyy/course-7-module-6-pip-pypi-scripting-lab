7d36d546737c2b486a1859e0efc8fdde5d291577
txmzyyy <tamaramk08@gmail.com>
2026-06-13 19:42:51 +0300

    complete code for pip,pypi lab

--- lib/generate_log.py
+++ lib/generate_log.py
@@ -1,19 +1,57 @@
 from datetime import datetime
 import os
+from urllib import response
+import requests
 
 def generate_log(data):
     # TODO: Implement log generation logic
 
-    # STEP 1: Validate input
-    # Hint: Check if data is a list
+    def fetch_data():
+    #fetching data from an API
+     response = requests.get(
+        "https://jsonplaceholder.typicode.com/posts/1"
+    )
 
-    # STEP 2: Generate a filename with today's date (e.g., "log_20250408.txt")
-    # Hint: Use datetime.now().strftime("%Y%m%d")
+    if response.status_code == 200:
+        return response.json()
 
-    # STEP 3: Write the log entries to a file using File I/O
-    # Use a with open() block and write each line from the data list
-    # Example: file.write(f"{entry}\n")
+    print("Failed to fetch data.")
+    return {}
 
-    # STEP 4: Print a confirmation message with the filename
 
-    pass
+def write_log(post):
+    #Writeing log data to a file containing a timestamp
+    filename = f"log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
+
+    with open(filename, "w") as file:
+        file.write("=== Automation Log ===\n")
+        file.write(
+            f"Generated: {datetime.now()}\n\n"
+        )
+
+        file.write(
+            f"Post ID: {post.get('id', 'N/A')}\n"
+        )
+        file.write(
+            f"Title: {post.get('title', 'No title')}\n"
+        )
+        file.write(
+            f"Body: {post.get('body', 'No content')}\n"
+        )
+
+    print(f"Log written to {filename}")
+
+
+if __name__ == "__main__":
+    print("Fetching data from API...")
+
+    post = fetch_data()
+
+    if post:
+        print(
+            "Fetched Post Title:",
+            post.get("title")
+        )
+        write_log(post)
+
+    print("Script completed successfully.")
